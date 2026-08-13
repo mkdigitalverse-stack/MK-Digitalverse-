014563dd5bc61a0d1dbbafa8eb9fe4ba25b08ebc
mkdigitalverse-stack <mkdigitalverse@gmail.com>
2026-08-13 13:07:30 +0530

    Use environment variable for server port
    Server txt update

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { NotificationEngine } from './src/services/serverNotificationEngine.js';
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json());
 
